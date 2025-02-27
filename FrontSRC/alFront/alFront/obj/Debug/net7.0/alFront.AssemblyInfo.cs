cac08aa3537224c73eba16d40e514e40595adf6e
JuhBeca <julliarebeca07@gmail.com>
2025-02-27 10:15:48 -0300

    a

--- FrontSRC/alFront/alFront/obj/Debug/net7.0/alFront.AssemblyInfo.cs
+++ FrontSRC/alFront/alFront/obj/Debug/net7.0/alFront.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("alFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45769213a56d6fb50267645795510ec0ab69abda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eb4104ec89b46bafb6707c9c8ebb774d7ec95b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("alFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("alFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
